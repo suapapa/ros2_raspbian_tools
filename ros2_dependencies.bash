bde091cbe15d7580d648b86bf7a1dd422ae836d6
Homin Lee <homin.lee@suapapa.net>
2020-10-23 15:16:28 +0900

    update key for ros repository

--- ros2_dependencies.bash
+++ ros2_dependencies.bash
@@ -7,7 +7,7 @@ apt update
 apt -y --no-install-recommends install dirmngr
 
 echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -cs` main" | tee /etc/apt/sources.list.d/ros-latest.list
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 apt update
 apt -y install git wget
